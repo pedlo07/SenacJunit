6532a2f9ad667748a0633ae497e48b5a0ffb9d0b
pedlo07 <pedroalmeidaveber@gmail.com>
2023-06-26 20:03:39 -0300

    Initial commit

---
+++ src/main/java/com/senac/Calculadora.java
@@ -0,0 +1,16 @@
+package com.senac;
+
+public class Calculadora {
+    public static int soma(int x, int y ) {
+        return x + y;
+    }
+    public static int subtracao(int x, int y ) {
+        return x - y;
+    }
+    public static int multiplicacao(int x, int y ) {
+        return x * y;
+    }
+    public static int divisao(int x, int y ) {
+        return x / y;
+    }
+}
---
+++ src/test/java/com/senac/CalculadoraTest.java
@@ -0,0 +1,32 @@
+package com.senac;
+
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+
+    @org.junit.jupiter.api.Test
+    void soma() {
+        int resultado = Calculadora.soma(2,2);
+        Assertions.assertEquals(4, resultado);
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtracao() {
+        int resultado = Calculadora.subtracao(8,6);
+        Assertions.assertEquals(2, resultado);
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiplicacao() {
+        int resultado = Calculadora.multiplicacao(4,6);
+        Assertions.assertEquals(24, resultado);
+    }
+
+    @org.junit.jupiter.api.Test
+    void divisao() {
+        int resultado = Calculadora.divisao(8,2);
+        Assertions.assertEquals(4, resultado);
+    }
+}
